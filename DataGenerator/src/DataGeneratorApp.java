f2bc117ae0d3192041bb7ee35c2030c35e472ec0
MariuszPe1 <mariusz.pela@t-media.pl>
2018-09-04 14:33:09 +0200

    User repeating bug fixed

--- DataGenerator/src/DataGeneratorApp.java
+++ DataGenerator/src/DataGeneratorApp.java
@@ -1,22 +1,30 @@
 import ReadFile.ReadJsonFile;
 import UserData.User;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class DataGeneratorApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
         Scanner howManyEntriesScanner = new Scanner(System.in);
         System.out.print("Enter number of users to generate: ");
         int howManyEntries = howManyEntriesScanner.nextInt();
 
-        //int howManyEntries = 2;
+
         ReadJsonFile entry = new ReadJsonFile();
-        int[] randomNumbers = entry.randomNumbers(howManyEntries);
+        HashSet<Integer> bbb = entry.randomNumbers(howManyEntries);
+
+        Integer[] randomNumbers = new Integer[bbb.size()];
+        for (int i = 0; i < bbb.size(); i++) {
+            randomNumbers = bbb.toArray(new Integer[0]);
+        }
+
+
         User[] users = entry.userData(howManyEntries, randomNumbers);
-        for (int i = 0; i < howManyEntries ; i++) {
-            System.out.println(randomNumbers[i] + " " + users[i]);
+        for (int i = 0; i < howManyEntries; i++) {
+            System.out.println(randomNumbers[i] + ". " + users[i]);
         }
 
 //        System.out.println(users[1].getName().getFirstName());
--- DataGenerator/src/ReadFile/ReadJsonFile.java
+++ DataGenerator/src/ReadFile/ReadJsonFile.java
@@ -8,48 +8,56 @@ import javax.json.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.HashSet;
+
 
 public class ReadJsonFile {
 
-    public int[] randomNumbers(int howManyEntries) {
-        int[] randomEntry = new int[howManyEntries];
-        for (int i = 0; i < howManyEntries; i++) {
-            randomEntry[i] = (int) Math.round(Math.random()*100);
-            //System.out.print(randomEntry[i] + " ");
+    public HashSet<Integer> randomNumbers(int howManyEntries) {
+//        int[] randomEntry = new int[howManyEntries];
+//        for (int i = 0; i < howManyEntries; i++) {
+//            randomEntry[i] = (int) Math.round(Math.random()*100);
+//            //System.out.print(randomEntry[i] + " ");
+//        }
+        HashSet<Integer> randomEntry = new HashSet<Integer>();
+        while (randomEntry.size() < howManyEntries) {
+            randomEntry.add((int) Math.round(Math.random() * 100));
         }
+
+
         return randomEntry;
     }
 
-    public User[] userData(int howManyEntries, int[] randomEntry) {
+    public User[] userData(int howManyEntries, Integer[] randomEntry) {
         User[] user = new User[howManyEntries];
-            try {
-                InputStream is = new FileInputStream("registration-data-file.json");
-                JsonReader jsonReader = Json.createReader(is);
-                JsonObject jsonObject = jsonReader.readObject();
-                JsonArray results = jsonObject.getJsonArray("data");
+        try {
+            InputStream is = new FileInputStream("registration-data-file.json");
+            JsonReader jsonReader = Json.createReader(is);
+            JsonObject jsonObject = jsonReader.readObject();
+            JsonArray results = jsonObject.getJsonArray("data");
 
 
             //0 - firstname, 1 - lastname, 2 - country, 3 - stateProvince, 4 - email  5 - pass
 
-                int i = 0;
-                while(i < howManyEntries) {
-                    String singleUser = results.get(randomEntry[i]).toString();
-                    String singleUserPureData = singleUser.replaceAll("[\\[\\]{}()\"]", "");
-                    String[] userData = singleUserPureData.split(",");
+            int i = 0;
+            while (i < howManyEntries) {
+                String singleUser = results.get(randomEntry[i]).toString();
+                String singleUserPureData = singleUser.replaceAll("[\\[\\]{}()\"]", "");
+                String[] userData = singleUserPureData.split(",");
 
-                    user[i] = new User(
-                            new Name(userData[0], userData[1]),
-                            new Address(userData[2], userData[3]),
-                            userData[4],
-                            userData[5]);
-                    i++;
-                }
-
-            } catch (FileNotFoundException e) {
-                System.out.println("File not found");
-            } catch (JsonException w) {
-                System.out.println(w.getMessage());
+                user[i] = new User(
+                        new Name(userData[0], userData[1]),
+                        new Address(userData[2], userData[3]),
+                        userData[4],
+                        userData[5]);
+                i++;
             }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (JsonException w) {
+            System.out.println(w.getMessage());
+        }
 
         return user;
     }
