83feebcfc86e864fdd1aa885396418805a384974
MariuszPe <mariusz.pela1@gmail.com>
2018-09-06 14:06:58 +0200

    XML support added

--- DataGenerator/src/DataGeneratorApp.java
+++ DataGenerator/src/DataGeneratorApp.java
@@ -1,19 +1,17 @@
-import ReadFile.ReadJsonFile;
+import ReadFile.ReadFileClass;
 import UserData.User;
-
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class DataGeneratorApp {
     public static void main(String[] args) {
 
-
         Scanner howManyEntriesScanner = new Scanner(System.in);
         System.out.print("Enter number of users to generate: ");
         int howManyEntries = howManyEntriesScanner.nextInt();
+//        int howManyEntries = 100;
 
-
-        ReadJsonFile entry = new ReadJsonFile();
+        ReadFileClass entry = new ReadFileClass();
         HashSet<Integer> randomNumberHashList = entry.randomNumbers(howManyEntries);
 
         Integer[] randomNumbersArray = new Integer[randomNumberHashList.size()];
@@ -21,18 +19,17 @@ public class DataGeneratorApp {
             randomNumbersArray = randomNumberHashList.toArray(new Integer[0]);
         }
 
-
         User[] users = entry.userData(howManyEntries, randomNumbersArray);
         for (int i = 0; i < howManyEntries; i++) {
             System.out.println(randomNumbersArray[i] + ". " + users[i]);
         }
 
-//        System.out.println(users[1].getName().getFirstName());
-//        System.out.println(users[1].getName().getLastName());
-//        System.out.println(users[1].getAddress().getCountry());
-//        System.out.println(users[1].getAddress().getStateProvince());
-//        System.out.println(users[1].getEmail());
-//        System.out.println(users[1].getPassword());
+        System.out.println(users[1].getName().getFirstName());
+        System.out.println(users[1].getName().getLastName());
+        System.out.println(users[1].getAddress().getCountry());
+        System.out.println(users[1].getAddress().getStateProvince());
+        System.out.println(users[1].getEmail());
+        System.out.println(users[1].getPassword());
 
     }
 }
---
+++ DataGenerator/src/ReadFile/ReadFileClass.java
@@ -0,0 +1,110 @@
+package ReadFile;
+
+import UserData.Address;
+import UserData.Name;
+import UserData.User;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import javax.json.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.util.HashSet;
+import java.util.Random;
+
+
+public class ReadFileClass {
+
+    public HashSet<Integer> randomNumbers(int howManyEntries) {
+
+        HashSet<Integer> randomEntry = new HashSet<Integer>();
+        while (randomEntry.size() < howManyEntries) {
+            randomEntry.add(new Random().nextInt(100));
+        }
+        return randomEntry;
+    }
+
+
+    public User[] userData(int howManyEntries, Integer[] randomEntry) {
+
+        User[] user = new User[howManyEntries];
+        try {
+            File fileToRead = new File("registration-data-file.json");
+            InputStream inputStream = new BufferedInputStream(new FileInputStream(fileToRead));
+
+            String extensionTemp = fileToRead.toString();
+            int lastDotBeforeFileExtension = extensionTemp.lastIndexOf(".");
+            String extension = extensionTemp.substring(lastDotBeforeFileExtension + 1);
+
+
+            if (extension.equals("xml")) {
+                //Rozwiązanie dla xml
+
+                DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+                Document doc = docBuilder.parse(inputStream);
+                Element ele = doc.getDocumentElement();
+                NodeList nodeList = ele.getChildNodes();
+
+                int recordsQuantity = (nodeList.getLength() - 1) / 2;
+                String[] resultsXML = new String[recordsQuantity];
+
+                for (int i = 0; i < recordsQuantity; i++) {
+                    Node node = nodeList.item(i);
+                    node.getParentNode().removeChild(node);
+                    resultsXML[i] = nodeList.item(i).getTextContent().trim().replaceAll("\\s+", ",");
+                }
+
+                int i = 0;
+                while (i < howManyEntries) {
+                    String singleUser = resultsXML[randomEntry[i]];
+                    String[] userData = singleUser.split(",");
+                    user[i] = createUser(userData);
+                    i++;
+                }
+
+            } else if (extension.equals("json")) {
+
+                //Rozwiązanie dla JSON
+                JsonReader jsonReader = Json.createReader(inputStream);
+                JsonObject jsonObject = jsonReader.readObject();
+                JsonArray resultsJSON = jsonObject.getJsonArray("data");
+
+                int i = 0;
+                while (i < howManyEntries) {
+                    String singleUser = resultsJSON.get(randomEntry[i]).toString();
+                    String singleUserPureData = singleUser.replaceAll("[\\[\\]{}()\"]", "");
+                    String[] userData = singleUserPureData.split(",");
+
+                    user[i] = createUser(userData);
+                    i++;
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (ParserConfigurationException | SAXException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+//            0 - firstname, 1 - lastname, 2 - country, 3 - stateProvince, 4 - email  5 - pass
+    private User createUser(String[] userData) {
+
+        User userToReturn = new User(
+                new Name(userData[0], userData[1]),
+                new Address(userData[2], userData[3]),
+                userData[4],
+                userData[5]);
+
+        return userToReturn;
+    }
+
+}
