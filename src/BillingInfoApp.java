1ab1dd0a6a26d680dcc3473c1c0dd57b12ae884e
MariuszPe <mariusz.pela1@gmail.com>
2018-09-06 16:52:33 +0200

    XML support added

--- src/BillingInfoApp.java
+++ src/BillingInfoApp.java
@@ -1,4 +1,4 @@
-import ReadFile.ReadJsonFile;
+import ReadFile.ReadFileClass;
 import UserData.User;
 
 
@@ -9,13 +9,13 @@ public class BillingInfoApp {
 
     Scanner howManyEntriesScanner = new Scanner(System.in);
     System.out.print("Enter number of users to generate: ");
-    int howManyEntries = Integer.parseInt(howManyEntriesScanner.nextLine());
-//        int howManyEntries = 2;
+        int howManyEntries = Integer.parseInt(howManyEntriesScanner.nextLine());
 
-        ReadJsonFile entry = new ReadJsonFile();
+        ReadFileClass entry = new ReadFileClass();
         HashSet<Integer> randomNumberHashList = entry.randomNumbers(howManyEntries);
 
         Integer[] randomNumbersArray = new Integer[randomNumberHashList.size()];
+
         for (int i = 0; i < randomNumberHashList.size(); i++) {
             randomNumbersArray = randomNumberHashList.toArray(new Integer[0]);
         }
@@ -34,51 +34,11 @@ public class BillingInfoApp {
                 " 10 - Phone\n");
         System.out.print("Type numbers separated with coma (eg. 1,4,6): ");
         String[] dataToShow = (howManyEntriesScanner.nextLine().split(","));
-        int[] datadata = Arrays.asList(dataToShow).stream().mapToInt(Integer::parseInt).toArray();
+        int[] specifiedDataToShow = Arrays.asList(dataToShow).stream().mapToInt(Integer::parseInt).toArray();
+//        int[] specifiedDataToShow = {1,10};
 
         User[] users = entry.userData(howManyEntries, randomNumbersArray);
-        for (int i = 0; i < howManyEntries; i++) {
-//            System.out.println(randomNumbersArray[i] + ". " + users[i]);
-
-            for (int j = 0; j < datadata.length; j++) {
-
-
-                switch (datadata[j]) {
-                    case 1:
-                        System.out.println(users[i].getName().getFirstName());
-                        break;
-                    case 2:
-                        System.out.println(users[i].getName().getLastName());
-                        break;
-                    case 3:
-                        System.out.println(users[i].getCompany());
-                        break;
-                    case 4:
-                        System.out.println(users[i].getAddress().getStreet());
-                        break;
-                    case 5:
-                        System.out.println(users[i].getAddress().getCity());
-                        break;
-                    case 6:
-                        System.out.println(users[i].getAddress().getCountry());
-                        break;
-                    case 7:
-                        System.out.println(users[i].getAddress().getStateProvince());
-                        break;
-                    case 8:
-                        System.out.println(users[i].getAddress().getPostalCode());
-                        break;
-                    case 9:
-                        System.out.println(users[i].getEmail());
-                        break;
-                    case 10:
-                        System.out.println(users[i].getPhone());
-                        break;
-                }
-            }
-            System.out.println("------------");
-        }
-        howManyEntriesScanner.close();
+        entry.showSelectedData(users, specifiedDataToShow, howManyEntries);
 
 //       1 System.out.println(users[1].getName().getFirstName());
 //       2 System.out.println(users[1].getName().getLastName());
@@ -90,7 +50,6 @@ public class BillingInfoApp {
 //       8 System.out.println(users[1].getCompany());
 //       9 System.out.println(users[1].getEmail());
 //       10 System.out.println(users[1].getPhone());
-
-
+        howManyEntriesScanner.close();
     }
 }
---
+++ src/ReadFile/ReadFileClass.java
@@ -0,0 +1,154 @@
+package ReadFile;
+
+import UserData.Address;
+import UserData.Name;
+import UserData.User;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import javax.json.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.util.HashSet;
+import java.util.Random;
+
+
+public class ReadFileClass {
+
+    public HashSet<Integer> randomNumbers(int howManyEntries) {
+
+        HashSet<Integer> randomEntry = new HashSet<Integer>();
+        while (randomEntry.size() < howManyEntries) {
+            randomEntry.add(new Random().nextInt(100));
+        }
+        return randomEntry;
+    }
+
+    public User[] userData(int howManyEntries, Integer[] randomEntry) {
+        User[] user = new User[howManyEntries];
+        try {
+            File fileToRead = new File("billingInfo-data.xml");
+            InputStream inputStream = new BufferedInputStream(new FileInputStream(fileToRead));
+
+            String extensionTemp = fileToRead.toString();
+            int lastDotBeforeFileExtension = extensionTemp.lastIndexOf(".");
+            String extension = extensionTemp.substring(lastDotBeforeFileExtension + 1);
+
+
+            if (extension.equals("xml")) {
+                //Rozwiązanie dla XML
+
+                DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+                Document doc = docBuilder.parse(inputStream);
+                Element ele = doc.getDocumentElement();
+                NodeList nodeList = ele.getChildNodes();
+
+                int recordsQuantity = (nodeList.getLength() - 1) / 2;
+                String[] resultsXML = new String[recordsQuantity];
+
+                for (int i = 0; i < recordsQuantity; i++) {
+                    Node node = nodeList.item(i);
+                    node.getParentNode().removeChild(node);
+                    resultsXML[i] = nodeList.item(i).getTextContent().replaceAll("^\\s+|\\s+$|", "").replaceAll("\\s{2,}", ",");
+                }
+
+                int i = 0;
+                while (i < howManyEntries) {
+                    String singleUser = resultsXML[randomEntry[i]];
+                    String[] userData = singleUser.split(",");
+                    user[i] = createUser(userData);
+                    i++;
+                }
+
+            } else if (extension.equals("json")) {
+                //Rozwiązanie dla JSON
+
+                JsonReader jsonReader = Json.createReader(inputStream);
+                JsonObject jsonObject = jsonReader.readObject();
+                JsonArray resultsJSON = jsonObject.getJsonArray("data");
+
+                int i = 0;
+                while (i < howManyEntries) {
+                    String singleUser = resultsJSON.get(randomEntry[i]).toString();
+                    String singleUserPureData = singleUser.replaceAll("[\\[\\]{}()\"]", "");
+                    String[] userData = singleUserPureData.split(",");
+
+                    user[i] = createUser(userData);
+                    i++;
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (ParserConfigurationException | SAXException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+
+    private User createUser(String[] userData) {
+
+        User userToReturn = new User(
+                new Name(userData[0], userData[1]),
+                new Address(userData[3], userData[4], userData[5], userData[6], userData[7]),
+                userData[2],
+                userData[8],
+                userData[9]);
+
+        return userToReturn;
+    }
+
+
+
+    public static void showSelectedData(User[] users, int[] specifiedDataToShow, int howManyEntries) {
+        for (int i = 0; i < howManyEntries; i++) {
+
+            for (int j = 0; j < specifiedDataToShow.length; j++) {
+
+                switch (specifiedDataToShow[j]) {
+                    case 1:
+                        System.out.println(users[i].getName().getFirstName());
+                        break;
+                    case 2:
+                        System.out.println(users[i].getName().getLastName());
+                        break;
+                    case 3:
+                        System.out.println(users[i].getCompany());
+                        break;
+                    case 4:
+                        System.out.println(users[i].getAddress().getStreet());
+                        break;
+                    case 5:
+                        System.out.println(users[i].getAddress().getCity());
+                        break;
+                    case 6:
+                        System.out.println(users[i].getAddress().getCountry());
+                        break;
+                    case 7:
+                        System.out.println(users[i].getAddress().getStateProvince());
+                        break;
+                    case 8:
+                        System.out.println(users[i].getAddress().getPostalCode());
+                        break;
+                    case 9:
+                        System.out.println(users[i].getEmail());
+                        break;
+                    case 10:
+                        System.out.println(users[i].getPhone());
+                        break;
+                }
+            }
+            System.out.println("------------");
+        }
+    }
+
+
+}
