528a546f4f3738557a8cd7268661a36b3c3a51f5
ewamil <ewa.milczanowska@gmail.com>
2018-10-19 16:25:02 +0200

    some code

--- src/main/java/com/infoshare/mteam/pages/HomePage.java
+++ src/main/java/com/infoshare/mteam/pages/HomePage.java
@@ -1,10 +1,20 @@
 package com.infoshare.mteam.pages;
 
 import com.infoshare.mteam.utils.waits.Waits;
+import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
 
 public class HomePage {
 
@@ -15,8 +25,68 @@ public class HomePage {
         PageFactory.initElements(driver, this);
     }
 
+    @FindBy(xpath = "//*[@id=\"featuredItemsContainer\"]/div[1]/div[2]/div/div/a")
+    private WebElement addToCartButton;
+
+    @FindBy(xpath = "//*[@id=\"featuredItemsContainer\"]/div[1]/div[1]/a")
+    private WebElement bagItem;
+
+
+    /////////////////////////////////////////////////////////////////////
+
+
     @FindBy(css = "#miniCartSummary > a")
     private WebElement shoppingCartIcon;
+
+    @FindBy(id = "featuredItemsContainer")
+    private List<WebElement> productList;
+
+    @FindBy(partialLinkText = "Checkout")
+    private WebElement checkoutButton;
+
+    @FindBy(xpath = "//*[@id=\"main_h\"]/div/div/div/div")
+    private List<WebElement> mainMenuTabs;
+
+    @FindBy(id = "productsContainer")
+    private WebElement productsContainer;
+
+    @FindBy(xpath = "//*[@id=\"featuredItemsContainer\"]/div[1]/div[2]/div/div/a")
+    private WebElement foo;
+
+
+    public void scrollDown () throws Exception {
+
+        Actions a = new Actions(driver);
+        a.moveToElement(bagItem);
+    }
+
+    public void clickOnParticularItem () {
+
+        bagItem.click();
+
+        Waits wait = new Waits(driver);
+        wait.waitForElementToBeVisible(addToCartButton);
+
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL, "http://demo.shopizer.com:8080/shop/product/vintage-courier-bag.html");
+
+    }
+
+    public void addToCart () {
+
+        addToCartButton.click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
 
     public void clickOnShoppingCart () {
 
@@ -24,4 +94,60 @@ public class HomePage {
         wait.waitForElementToBeVisible(shoppingCartIcon);
         shoppingCartIcon.click();
     }
+
+    public void getProductListSize () {
+
+        int productListSize = productList.size();
+        System.out.println("***************************************product list size: " + productListSize);
+    }
+
+
+
+//    public void clickBagsTab () {
+//        mainMenuTabs.get(3).click();
+//        Waits waits = new Waits(driver);
+//        waits.waitForElementToBeVisible(productsContainer);
+//        assertTrue("product is not visible", productsContainer.isDisplayed());
+//    }
+
+
+
+
+
+//    public void clickAddToCart (int productIndex) {
+//
+////        Waits waits = new Waits(driver);
+////        waits.waitForElementToBeVisible(productList.get(productIndex));
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        this.scrollDown();
+//        foo.click();
+////        productList.get(productIndex).click();
+//    }
+
+
+
+
+//    public void clickAddToCartOnAllProductsFromList (int productIndex) {
+//
+//        for (WebElement product : productList) {
+//            productList.get(productIndex).click();
+//        }
+//    }
+
+//    public void scrollDown () {
+//        JavascriptExecutor jse = (JavascriptExecutor) driver;
+//        jse.executeScript("window.scrollBy(0, 500);");
+//    }
+
+    public void checkIfCheckoutButtonVisible () {
+        Waits wait = new Waits(driver);
+        wait.waitForElementToBeVisible(checkoutButton);
+        boolean checkoutButtonDisplayed = checkoutButton.isDisplayed();
+        System.out.println("checkoutButtonDisplayed" + checkoutButtonDisplayed);
+    }
+
 }
--- src/test/java/com/infoshare/mteam/ui/CartTest.java
+++ src/test/java/com/infoshare/mteam/ui/CartTest.java
@@ -1,16 +1,22 @@
 package com.infoshare.mteam.ui;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import com.infoshare.mteam.pages.HomePage;
 import com.infoshare.mteam.utils.waits.Waits;
 
 import com.infoshare.mteam.utils.driver.WebDriverCreators;
 import com.infoshare.mteam.utils.driver.WebDriverProvider;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.Assert.*;
 
@@ -19,7 +25,7 @@ public class CartTest {
 
     private static final String PAGE_URL = "http://demo.shopizer.com:8080/";
 
-    private WebDriver driver;
+    private static WebDriver driver;
 
     private HomePage homePage;
 
@@ -36,10 +42,102 @@ public class CartTest {
         driver.get(PAGE_URL);
     }
 
-    @Test
-    public void clickChartIconAndcheckIfEmpty() {
-        //uzupenij elementem!!
-//        waits.waitForElementToBeVisible();
-        homePage.clickOnShoppingCart();
+    @After
+    public void tearDown() {
+        driver.close();
     }
+
+    @Test
+    public void testAddToCart () {
+
+        driver.get(PAGE_URL);
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main_h\"]/div/div/div/div/nav/ul/li[1]/a")));
+        element.click();
+
+        WebElement bagsList = driver.findElement(By.className("products-grid"));
+        String URL = driver.getCurrentUrl();
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" + URL);
+        Assert.assertEquals(URL, "http://demo.shopizer.com:8080/shop/category/handbags.html/ref=c:1");
+        Assert.assertTrue(driver.getTitle().equals("Vintage handbags"));
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+//    @Test
+//    public void clickCartIconAndcheckIfEmpty() {
+//
+////        homePage.clickOnShoppingCart();
+////        homePage.getProductListSize();
+////        homePage.clickBagsTab();
+////        homePage.clickAddToCart(0);
+//        homePage.getProductListSize();
+////        homePage.clickAddToCart(0);
+////        homePage.clickOnShoppingCart();
+////        homePage.checkIfCheckoutButtonVisible();
+//    }
+//
+//    @Test
+//    public void clickAnItemAndAddToCart () throws Exception {
+//
+//        WebElement bagItem = driver.findElement(By.cssSelector("#featuredItemsContainer > div:nth-child(1) > div.thumbnail.product-img > a"));
+//
+//        homePage.scrollDown();
+//        homePage.clickOnParticularItem();
+//        homePage.addToCart();
+//    }
+//
+////    @Test
+////    public void singleProductAddToCart () {
+////
+////    }
+////
+////    @Test
+////    public void multipleProductsAddToCart () {
+////
+////    }
+////
+////    @Test
+////    public void clearCart () {
+////
+////    }
+////
+////    @Test
+////    public void editQuantityValidationCart () {
+////
+////    }
+////
+////    @Test
+////    public void editQuantityCartBoundryValue () {
+////
+////    }
+////
+////    @Test
+////    public void singleProductRemoveFromCart () {
+////
+////    }
+////
+////    @Test
+////    public void multipleProductsRemoveFromCart () {
+////
+////    }
+////
+////    @Test
+////    public void continueShopping () {
+////
+////    }
+
+
 }
