39e4b449e9f8914043e559e6038f307727103218
Maciej Wojciechowski <maciej.wojciechowski@outlook.com>
2018-10-21 20:52:05 +0200

    added BasePage class

--- src/main/java/com/infoshare/mteam/pages/HomePage.java
+++ src/main/java/com/infoshare/mteam/pages/HomePage.java
@@ -3,11 +3,9 @@ package com.infoshare.mteam.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class HomePage {
+public class HomePage extends BasePage{
 
     @FindBy(id = "customerAccount")
     private WebElement menuMyAccount;
@@ -15,13 +13,8 @@ public class HomePage {
     @FindBy(xpath = "//a[text()='Sign in']")
     private WebElement linkSignIn;
 
-    private WebDriver driver;
-    private WebDriverWait wait;
-
     public HomePage(WebDriver driver){
-        this.driver = driver;
-        wait = new WebDriverWait(driver, 10);
-        PageFactory.initElements(driver, this);
+        super(driver);
     }
 
     public void clickSignIn(){
